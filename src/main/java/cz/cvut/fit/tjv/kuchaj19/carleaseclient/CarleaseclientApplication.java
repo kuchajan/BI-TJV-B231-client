1fc178350ff4eb4e426857b20da163b652f8ee68
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-12-27 13:16:06 +0100

    Initial commit

---
+++ src/main/java/cz/cvut/fit/tjv/kuchaj19/carleaseclient/CarleaseclientApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.kuchaj19.carleaseclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarleaseclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarleaseclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/tjv/kuchaj19/carleaseclient/CarleaseclientApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.kuchaj19.carleaseclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarleaseclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
